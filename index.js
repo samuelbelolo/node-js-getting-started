a71e8565651e518c77506bbff09bcd58cc90401b
samuelbelolo <samuel.belolo@hetic.net>
2020-12-25 23:54:37 +0100

    testing

--- index.js
+++ index.js
@@ -6,56 +6,7 @@ const PORT = process.env.PORT || 5000;
 const phantom = require("phantom");
 const path = require("path");
 
-express()
-  .get("/test", (req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
-  })
-  .get("/", (req, res) => {
-    res.json({ error: true, message: "need a slug" });
-  })
-  .get("/:slug", async (req, res) => {
-    try {
-      const infos = {};
-      await getPage(req.params.slug); // get page
-      const data = await getList(req.params.slug); // Get memories
 
-      const instance = await phantom.create();
-      const page = await instance.createPage();
-      await page.on("onResourceRequested", function (requestData) {
-        console.info("Requesting", requestData.url);
-      });
-      await page.open(`${req.protocol}://${req.get("host")}/test`);
-      infos.ceremonyNumber = await getContentNumber(data.ceremony, page);
-      infos.memoriesNumber = await getContentNumber(data.memory, page);
-      const result = {};
-      result.slug = req.params.slug;
-      const infosTomes = getTomesInfos(infos);
-
-      if (infosTomes.length === 3) {
-        result.size = "custom";
-      } else if (infosTomes.length === 2) {
-        if (infosTomes[1].total <= 54) {
-          result.size = "ls";
-        } else if (infosTomes[1].total <= 94) {
-          result.size = "lm";
-        } else if (infosTomes[1].total <= 188) {
-          result.size = "ll";
-        }
-      } else {
-        if (infosTomes[0].total <= 54) {
-          result.size = "s";
-        } else if (infosTomes[0].total <= 94) {
-          result.size = "m";
-        } else if (infosTomes[0].total <= 188) {
-          result.size = "l";
-        }
-      }
-      res.json(result);
-    } catch (error) {
-      res.json(error);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const getPage = async (slug) => {
   const url = `https://api.inmemori.com/pages/${slug}`;
@@ -82,13 +33,59 @@ const getList = async (slug) => {
       });
   });
 };
+const getInfos = async (req, res) => {
+  try {
+    const accurate = req.query.accurate === "true" ? true : false
+    const infos = {};
+    await getPage(req.params.slug); // get page
+    const data = await getList(req.params.slug); // Get memories
 
-const getContentNumber = async (array, page) => {
+    const instance = await phantom.create();
+    const page = await instance.createPage();
+    await page.on("onResourceRequested", function (requestData) {
+      console.info("Requesting", requestData.url);
+    });
+    await page.open(`${req.protocol}://${req.get("host")}/test`);
+    infos.ceremonyNumber = await getContentNumber(data.ceremony, page, accurate);
+    infos.memoriesNumber = await getContentNumber(data.memory, page, accurate);
+    const result = {};
+    result.slug = req.params.slug;
+    const infosTomes = getTomesInfos(infos);
+
+    if (infosTomes.length === 3) {
+      result.size = "custom";
+    } else if (infosTomes.length === 2) {
+      if (infosTomes[1].total <= 54) {
+        result.size = "ls";
+      } else if (infosTomes[1].total <= 94) {
+        result.size = "lm";
+      } else if (infosTomes[1].total <= 188) {
+        result.size = "ll";
+      }
+    } else {
+      if (infosTomes[0].total <= 54) {
+        result.size = "s";
+      } else if (infosTomes[0].total <= 94) {
+        result.size = "m";
+      } else if (infosTomes[0].total <= 188) {
+        result.size = "l";
+      }
+    }
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
+};
+const getContentNumber = async (array, page, accurate) => {
   let i = 0;
   for (const element of array) {
     if (element.message != "") {
-      element.message = await cutSentence(page, element.message); // check if the message is not higher than normal pages
-      i += element.message.length; // add numbers of pages for the text
+      if (accurate) {
+        element.message = await cutSentence(page, element.message); // check if the message is not higher than normal pages
+        i += element.message.length; // add numbers of pages for the text
+      } else {
+        i++;
+      }
     }
     i += element.images.length;
   }
@@ -204,3 +201,14 @@ const getTomesInfos = (infos) => {
   }
   return array;
 };
+
+
+express()
+  .get("/test", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
+  })
+  .get("/", (req, res) => {
+    res.json({ error: true, message: "need a slug" });
+  })
+  .get("/:slug", getInfos)
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
